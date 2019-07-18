063a7162a782ce3d6021fba5800d3e3199749270
bs4it <fernando.dellatorre@bs4it.com.br>
2019-07-18 09:22:47 -0300

    Carga Inicial

---
+++ 01-lvm.sh
@@ -0,0 +1,132 @@
+#!/bin/bash
+clear
+echo "############################################################################"
+echo "CUIDADO AO USAR ESTE SCRIPT, O DISCO INDICADO SERA SUMARIAMENTE FORMATADO!!!"
+echo "############################################################################"
+echo " "
+echo "Para interromper este script pressione ctrl+c."
+while [[ $accept != "YES" ]]
+do
+  read -p "Para prosseguir digite 'YES': " accept
+done
+clear
+echo "############################################################################"
+echo "CUIDADO AO USAR ESTE SCRIPT, O DISCO INDICADO SERA SUMARIAMENTE FORMATADO!!!"
+echo "############################################################################"
+echo " "
+echo "Para interromper este script pressione ctrl+c."
+echo " "
+echo "Os seguintes dispositivos de bloco estao presentes:"
+echo " "
+lsblk -o NAME,SIZE,FSTYPE,MOUNTPOINT,MODEL
+#lsblk -f
+echo " "
+echo "O dispositivo a ser usado NAO deve conter particoes, volumes ou filesystems!"
+read -p "Pressione ENTER para seguir"
+clear
+echo "############################################################################"
+echo "CUIDADO AO USAR ESTE SCRIPT, O DISCO INDICADO SERA SUMARIAMENTE FORMATADO!!!"
+echo "############################################################################"
+echo " "
+echo "Para interromper este script pressione ctrl+c."
+echo " "
+echo "Abaixo a lista de dispositivos SCSI para maiores detalhes:"
+lsblk -S
+echo " "
+while [[ -z "$blkdevice" ]]
+do
+	read -p "Digite o nome do dispositivo a ser usado (ex. sdb): " blkdevice
+done
+echo "TODO O CONTEUDO DO DISPOSITIVO $blkdevice SERA PERMANENTEMENTE ELIMINADO"
+echo "ESTE PROCESSO NAO PODERA SER REVERTIDO"
+echo "O DISCO $blkdevice DEVE ESTAR VAZIO PARA MAIOR SEGURANCA".
+echo " "
+read -p "Para prosseguir digite novamente o nome do dispositivo: " confirmdevice
+if [ $confirmdevice != $blkdevice ]; then
+	echo "Confirmacao incorreta, encerrando o script"
+	exit 1
+fi
+clear
+echo "############################################################################"
+echo "O DISCO $blkdevice SERA APAGADO E RECEBERA A NOVA ESTRUTURA DE LVM"
+echo "############################################################################"
+echo " "
+echo " "
+confirmationcode=`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 6 | head -n 1`
+read -p "Para realmente continuar digite '$confirmationcode' :" typedconfirmationcode
+if [ $typedconfirmationcode != $confirmationcode ]; then
+        echo "Codigo incorreto, encerrando o script"
+        exit 1
+fi
+echo " "
+echo "Criando volume fisico:"
+pvcreate /dev/$blkdevice
+result=$?
+if [ $result != 0 ]; then
+	echo "Falha ao criar volume fisico - erro $result"
+	echo "O disco pode conter particoes ou ja participar de um VG. Abortando"
+	exit 1
+fi
+echo " "
+echo "Criando volume group:"
+vgcreate vg_veeambackup /dev/$blkdevice
+result=$?
+if [ $result != 0 ]; then
+	echo "Falha ao criar volume group - erro $result"
+	echo "Corrija erros e execute novamente. Abortando"
+	exit 1
+fi
+echo " "
+echo "Criando logical volume usando todo espaco disponivel:"
+lvcreate -l 100%FREE -n lv_veeambackup vg_veeambackup
+result=$?
+if [ $result != 0 ]; then
+	echo "Falha ao criar logical volume - erro $result"
+	echo "Corrija erros e execute novamente. Abortando"
+	exit 1
+fi
+echo " "
+echo "Criando File System com suporte a arquivos grandes:"
+mkfs.ext4 -L largefile4 /dev/vg_veeambackup/lv_veeambackup
+result=$?
+if [ $result != 0 ]; then
+	echo "Falha ao criar sistema de arquivos - erro $result"
+	echo "Corrija erros e execute novamente. Abortando"
+	exit 1
+fi
+echo " "
+echo "Checando se o fstab já contem o mount point..."
+fstabmount=`grep /dev/mapper/vg_veeambackup-lv_veeambackup /etc/fstab`
+if [ -z $fstabmount ]; then
+	mountpointdefault="/veeambackup"
+	echo "Insira o path onde o volume devera ser montado (Ex. /veeambackup): "
+	read mountpoint
+	if [ -z $mountpoint ]
+	then
+	        mountpoint=$mountpointdefault
+	fi
+	echo "Criando a pasta $mountpoint..."
+	mkdir -p $mountpoint
+	sleep 1
+        echo "Inserindo mount mount no arquivo /etc/fstab:"
+	echo "/dev/mapper/vg_veeambackup-lv_veeambackup $mountpoint ext4    defaults        0       0" >> /etc/fstab
+else
+        echo "Mount point ja existente, o arquivo /etc/fstab nao sera alterado."
+fi
+echo " "
+echo "Montando todos os filesystems em /etc/fstab..."
+mount -a
+result=$?
+if [ $result != 0 ]; then
+        echo "Falha ao montar filesystems - erro $result"
+        echo "Verifique fstab"
+else
+	echo "Filesystems montados com sucesso."
+	echo "novo volume montado com sucesso em `grep /dev/mapper/vg_veeambackup-lv_veeambackup /etc/fstab | cut -d " " -f 2`."
+fi
+echo " "
+echo " "
+echo "Processo concluido"
+echo " "
+
+
---
+++ 02-user_cert.sh
@@ -0,0 +1,112 @@
+#!/bin/bash
+
+
+
+clear
+echo " "
+echo -e "\e[32mEASY VEEAM LINUX REPOSITORY SCRIPT - BS4IT\e[39m"
+echo " "
+echo " "
+echo " "
+echo "Instalar pacotes necessarios?"
+while [[ -z $installpkgs ]]
+do
+	echo -e "Digite \e[97mS\e[39m ou \e[97mN\e[39m:"
+	read installpkgs
+done
+if [ $installpkgs == "S" ] || [ $installpkgs == "s" ]; then
+	apt-get install build-essential -y
+	cpan constant Carp Cwd Data::Dumper Encode Encode::Alias Encode::Config Encode::Encoding Encode::MIME::Name Exporter Exporter::Heavy File::Path File::Spec File::Spec::Unix File::Temp List::Util Scalar::Util Socket Storable threads
+echo " "
+echo "Pressione Enter para continuar:"
+read
+clear
+echo " "
+echo -e "\e[32mEASY VEEAM LINUX REPOSITORY SCRIPT - BS4IT\e[39m"
+echo " "
+echo " "
+echo " "
+fi
+
+usernamedefault=veeambackup
+echo "Insira o nome do usuario (veeambackup):"
+read username
+if [ -z $username ]
+then
+        username=$usernamedefault
+fi
+
+mountpointdefault="/veeambackup"
+echo "Insira o mountpoint para o repositorio (/veeambackup):"
+read mountpoint
+if [ -z $mountpoint ]
+then
+	mountpoint=$mountpointdefault
+fi
+
+# gera numero aleatorio entre 11000 e 43767
+ssh_port=$(($RANDOM+11000))
+sed -i "/Port /c\Port $ssh_port" /etc/ssh/sshd_config
+/etc/init.d/ssh restart
+ufw --force reset
+ufw --force enable
+ufw allow $ssh_port/tcp
+ufw allow 2500:5000/tcp
+useradd -s /bin/bash -b /var/lib -c "Veeam Linux user Backup" -m $username
+passwd=`openssl rand -base64 32`
+echo "$username:$passwd" | chpasswd
+passwd -l $username
+passphrase=`openssl rand -base64 32`
+su -l $username -c "rm -f /var/lib/$username/.ssh/*"
+su -l $username -c "ssh-keygen -f /var/lib/$username/.ssh/id_rsa -N $passphrase -C Key_for_Veeam_Linux_user_Backup -O no-x11-forwarding -O no-port-forwarding -O no-agent-forwarding"
+su -l $username -c "cat /var/lib/$username/.ssh/id_rsa.pub > /var/lib/$username/.ssh/authorized_keys"
+mkdir -p $mountpoint
+chown $username $mountpoint
+chmod 700 $mountpoint
+clear
+echo -e "\e[32m###################################################################"
+echo -e "Acesse http://`ifconfig | grep inet | grep -v inet6 | grep -v 127.0.0.1 | sed -e 's/^[[:space:]]*//' | cut -d " " -f 2`:4080,"
+echo -e "Para ter acesso aos dados da chave publica privada"
+echo -e ""
+echo -e "Ao concluir retorne a esta tela e pressione CTRL+C para prosseguir."
+echo -e "###################################################################\e[39m"
+echo '<html><head><meta http-equiv="Content-Type" content="text/html; charset=utf-8"><title>Dados de acesso ao repositorio.</title></head>' >index.html
+echo "<body>" >> index.html
+echo '<font face="Arial, Helvetica, sans-serif">' >> index.html
+echo "<h1>Dados de acesso ao repositorio:</h1>" >> index.html
+echo "<h3>Copie a partir da linha abaixo e cole no arquivo <b>private.pem</b>.</h3>" >> index.html
+echo "<p>" >> index.html
+echo "<pre>" >> index.html
+cat /var/lib/$username/.ssh/id_rsa >> index.html
+echo "</pre>" >> index.html
+echo "</p>" >> index.html
+echo "<p>" >> index.html
+echo "<h3>Copie ate a linha acima</h3>" >> index.html
+echo "</p>" >> index.html
+echo "<p>Realize o download do <a href="https://the.earth.li/~sgtatham/putty/latest/w32/puttygen.exe">PuttyGen</a> para converter a chave privada do formato PEM para o formato PPK, compatível com o Veeam.</p>" >> index.html
+echo "<p>" >> index.html
+echo "<b>Username:</b> $username<br>" >> index.html 
+echo "<b>Passphrase:</b> $passphrase<br>" >> index.html
+echo "<b>Porta SSH:</b>$ssh_port<br>" >> index.html
+echo "</font>" >> index.html
+echo "</body>" >> index.html
+echo "</html>" >> index.html
+echo " "
+python3 -m http.server 4080 && rm -f index.html
+clear
+echo "Limpando arquivos temporarios..."
+echo "Script finalizado."
+echo "Saindo em 5 segundos"
+sleep 1
+echo "Saindo em 4 segundos"
+sleep 1
+echo "Saindo em 3 segundos"
+sleep 1
+echo "Saindo em 2 segundos"
+sleep 1
+echo "Saindo em 1 segundos"
+sleep 1
+echo "Saindo em 0 segundos"
+exit
+
+
