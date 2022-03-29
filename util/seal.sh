3dbb8b3db7b9c801839edfbadca214545abd5017
Fernando Della Torre <fernando.dellatorre@bs4it.com.br>
2022-03-29 19:17:52 -0300

    Versao 2022 - teste

--- util/seal.sh
+++ util/seal.sh
@@ -1,45 +1,45 @@
 #!/bin/bash
 # 2022 - Fernando Della Torre @ BS4IT
 # prepare the s.o. for the customer
-echo "Copying issue file..."
+echo -e "\033[1;37mCopying issue file...\033[0m"
 cp $(dirname "$0")/issue /etc/issue
-echo "Setting GRUB..."
+echo -e "\033[1;37mSetting GRUB...\033[0m"
 sed -i 's/^GRUB_TIMEOUT=.*/GRUB_TIMEOUT=3/' /etc/default/grub 2>/dev/null
 sed -i 's/^GRUB_DISTRIBUTOR=.*/GRUB_DISTRIBUTOR="BS4IT - Veeam Linux Hardened Repository"/' /etc/default/grub 2>/dev/null
 sed -i 's/^GRUB_CMDLINE_LINUX_DEFAULT=.*/GRUB_CMDLINE_LINUX_DEFAULT=""/' /etc/default/grub 2>/dev/null
 update-grub2
-echo "Deleting log.gz files..."
+echo -e "\033[1;37mDeleting log.gz files...\033[0m"
 find /var/log/ -type f -name "*.gz" -exec rm -f {} \;
-echo "Truncating log files..."
+echo -e "\033[1;37mTruncating log files...\033[0m"
 find /var/log/ -type f -exec truncate -s 0 {} \;
-echo "Deleting temp files..."
+echo -e "\033[1;37mDeleting temp files...\033[0m"
 rm -rf /tmp/*
 rm -rf /var/tmp/*
-echo "Deleting network config files files..."
+echo -e "\033[1;37mDeleting network config files files...\033[0m"
 rm /etc/netplan/*.yaml
 rm /etc/network/interfaces
 rm /etc/network/interfaces.d/*
-echo "Cleaning APT cache..."
+echo -e "\033[1;37mCleaning APT cache...\033[0m"
 apt clean -y
-echo "Enabling network configuration wizard on next boot..."
+echo -e "\033[1;37mEnabling network configuration wizard on next boot...\033[0m"
 cp $(dirname "$0")/set_net.service /etc/systemd/system/
 systemctl enable set_net.service
-echo "Cleaning bash history..."
+echo -e "\033[1;37mCleaning bash history...\033[0m"
 bash -c "cat /dev/null > /root/.bash_history && history -c"
 (cat /dev/null > ~/.bash_history; history -c; history -w)
 accept=""
 while ! [[ $accept = 'Y' || $accept = 'y' || $accept = 'N' || $accept = 'n' ]]
 do
-	echo -n -e "Do you want to shutdown this machine right now? ${YELLOW}(Y/N)${NC}:"
+	echo -n -e "\033[1;37mDo you want to shutdown this machine right now? \033[1;33m(Y/N)\033[0m:"
 	read accept
 	case $accept in
 		y|Y)
-			echo "Shutting down..."
+			echo -e "\033[1;37mShutting down...\033[0m"
             sleep 1
             shutdown -h 0
 			;;
 		n|N)
-			echo "Quitting, bye!"
+			echo -e "\033[1;37mQuitting, bye!\033[0m"
 			exit 0
 			;;
   	esac
